f8a923d8776983fd7427ae01a4c36120a2cab38b
Santiago-scordia <santiscordia@est.frba.utn.edu.ar>
2022-03-25 19:29:50 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="navbar-section">
+      <div className='title'>
+      <img src={logo} className="navbar__logo" alt="logo" />
+      <h1>
+          Ǝ U ᒧ O ⅁ Y
+      </h1>
+      </div>
+      
+        <nav>
+            <ul class="nav__links">
+                <li class="nav__item"><a class="nav__item" href="#chicos">Chicos</a></li>
+                <li class="nav__item"><a class="nav__item" href="#medianos">Medianos</a></li>
+                <li class="nav__item"><a class="nav__item" href="#grandes">Grandes</a></li>
+            </ul>
+        </nav>
       </header>
-    </div>
+    </>
   );
 }
 
